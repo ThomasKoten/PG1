f285108f2909cb6f0900107eedfd72aa04712c9c
ThomasKoten <92688427+ThomasKoten@users.noreply.github.com>
2023-11-08 12:21:05 +0100

    Add files via upload

--- cviceni06/script.js
+++ cviceni06/script.js
@@ -1,12 +1,19 @@
-window.onload = function() {
+// const { vec2, vec3, mat3, mat4 } = glMatrix;
+window.onload = function () {
 	var gl = document.getElementById("webgl_canvas").getContext("experimental-webgl");
+	// 	var convertButton = document.getElementById("draw");
 
+
+	// 	convertButton.addEventListener('click', function(){
+	// 		make_globe(gl);
+	// 	  });
+	// }
+	// function make_globe(gl){
 	// Create vertex shader
 	var vertexShaderCode = document.querySelector("#vs").textContent;
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	gl.shaderSource(vertexShader, vertexShaderCode);
 	gl.compileShader(vertexShader);
-	
 	// Create fragment shader
 	var fragmentShaderCode = document.querySelector("#fs").textContent;
 	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -19,7 +26,6 @@ window.onload = function() {
 	gl.attachShader(program, fragmentShader);
 	gl.linkProgram(program);
 	gl.useProgram(program);
-
 	// Create buffer for positions of vertices
 	var posLoc = gl.getAttribLocation(program, "pos");
 	gl.enableVertexAttribArray(posLoc);
@@ -28,38 +34,31 @@ window.onload = function() {
 	gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
 	// We need many vertices, because each vertex need
 	// own value of normal and UV
-	var vertices = [
-		// Bottom face
-		-1.0, -1.0, -1.0,	// 0
-		 1.0, -1.0, -1.0,	// 1
-		 1.0,  1.0, -1.0,	// 2
-		-1.0,  1.0, -1.0,	// 3
-		// Left face
-		-1.0, -1.0, -1.0,	// 4
-		 1.0, -1.0, -1.0,	// 5
-		 1.0, -1.0,  1.0,	// 6
-		-1.0, -1.0,  1.0,	// 7
-		// Front face
-		 1.0, -1.0, -1.0,	// 8
-		 1.0,  1.0, -1.0,	// 9
-		 1.0,  1.0,  1.0,	// 10
-		 1.0, -1.0,  1.0,	// 11
-		// Right face
-		 1.0,  1.0, -1.0,	// 12
-		-1.0,  1.0, -1.0,	// 13
-		-1.0,  1.0,  1.0,	// 14
-		 1.0,  1.0,  1.0,	// 15
-		// Back face
-		-1.0, -1.0, -1.0,	// 16
-		-1.0, -1.0,  1.0,	// 17
-		-1.0,  1.0,  1.0,	// 18
-		-1.0,  1.0, -1.0,	// 19
-		// Top face
-		-1.0, -1.0,  1.0,	// 20
-		 1.0, -1.0,  1.0,	// 21
-		 1.0,  1.0,  1.0,	// 22
-		-1.0,  1.0,  1.0	// 23
-	];
+
+	const sphere_div = 15//Number(document.getElementById("sphere_div").value);
+	console.log(sphere_div)
+	var vertices = [];
+	var normals = [];
+	for (let m = 0; m <= sphere_div; m++) {
+		layer = Math.PI / 2 - m * Math.PI / sphere_div;
+		z = Math.sin(layer);
+		xy = Math.cos(layer);
+
+		for (let n = 0; n <= sphere_div; n++) {
+			point = n * 2* Math.PI / sphere_div;
+			x = xy*Math.cos(point);
+			y =xy* Math.sin(point);
+			vertices.push(x)
+			vertices.push(z)
+			vertices.push(y)
+
+			normals.push(x);
+			normals.push(y);
+			normals.push(z);
+			
+		}
+	}
+	console.log(vertices);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 0, 0);
 
@@ -68,38 +67,32 @@ window.onload = function() {
 	gl.enableVertexAttribArray(uvLoc);
 	var uvBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-	uvs = [
-		// Bottom face
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 1.0,  1.0,
-		 0.0,  1.0,
-		// Left face
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 1.0,  1.0,
-		 0.0,  1.0,
-		// Front
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 1.0,  1.0,
-		 0.0,  1.0,
-		// Right face
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 1.0,  1.0,
-		 0.0,  1.0,
-		// Back face
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 1.0,  1.0,
-		 0.0,  1.0,
-		// Top face
-		 0.0,  0.0,
-		 1.0,  0.0,
-		 1.0,  1.0,
-		 0.0,  1.0
-	];
+	uvs = []; //Tři body
+	for (let m = 0; m <= sphere_div; m++) {
+		for (let n = 0; n <= sphere_div; n++) {
+			u = n / sphere_div;
+			v = m / sphere_div;
+			uvs.push(u);
+			uvs.push(v);
+		}
+	}
+	// for (let m = 0; m <= sphere_div; m += 2) {
+	// 	for (let n = 0; n <= sphere_div; n += 2) {
+	// 		point1_x = n / sphere_div;
+	// 		point1_y = m / sphere_div;
+	// 		point2_x = (n + 1) / sphere_div;
+	// 		point2_y = m / sphere_div;
+	// 		point3_x = n / sphere_div;
+	// 		point3_y = (m + 1) / sphere_div;
+	// 		point4_x = (n + 1) / sphere_div;
+	// 		point4_y = (m + 1) / sphere_div;
+
+	// 		uvs.push(point1_x, point1_y, point2_x, point2_y, point3_x, point3_y)
+	// 		uvs.push(point2_x, point2_y, point3_x, point3_y, point4_x, point4_y)
+	// 	}
+	// }
+
+	console.log(uvs)
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW);
 	gl.vertexAttribPointer(uvLoc, 2, gl.FLOAT, false, 0, 0);
 
@@ -108,69 +101,54 @@ window.onload = function() {
 	gl.enableVertexAttribArray(normalLoc);
 	var normalBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-	normals = [
-		// Bottom face
-		 0.0,  0.0, -1.0,
-		 0.0,  0.0, -1.0,
-		 0.0,  0.0, -1.0,
-		 0.0,  0.0, -1.0,
-		// Left face
-		 0.0, -1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		// Front
-		 1.0,  0.0,  0.0,
-		 1.0,  0.0,  0.0,
-		 1.0,  0.0,  0.0,
-		 1.0,  0.0,  0.0,
-		// Right face
-		 0.0,  1.0,  0.0,
-		 0.0,  1.0,  0.0,
-		 0.0,  1.0,  0.0,
-		 0.0,  1.0,  0.0,
-		// Back face
-		-1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		// Top face
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0,  1.0
-	];
+	// for (let m = 0; m <= sphere_div; m++) {
+	// 	for (let n = 0; n <= sphere_div; n++) {
+	// 		var theta = m * Math.PI / sphere_div;
+	// 		var phi = n * 2 * Math.PI / sphere_div;
+
+	// 		var x = Math.sin(theta) * Math.cos(phi);
+	// 		var y = Math.sin(theta) * Math.sin(phi);
+	// 		var z = Math.cos(theta);
+
+			
+	// 	}
+	// }
+
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
 	gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, true, 0, 0);
 
 	// Create index buffer
 	var indexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-	var indices = [
-		 0,  1,  2,	// 0
-		 0,  2,  3,
-		 4,  5,  6,	// 1
-		 4,  6,  7,
-		 8,  9, 10,	// 2
-		 8, 10, 11,
-		12, 13, 14,	// 3
-		12, 14, 15,
-		16, 17, 18,	// 4
-		16, 18, 19,
-		20, 21, 22,	// 5
-		20, 22, 23
-	];
+	var indices = [];
+	for (let m = 0; m <= sphere_div; m++) {
+		for (let n = 0; n <= sphere_div; n++) {
+			p1 = m * (sphere_div) + n
+			p2 = (m + 1) * (sphere_div) + n
+
+			indices.push(p1)
+			indices.push(p1 + 1)
+			indices.push(p2)
+
+			indices.push(p2)
+			indices.push(p1 + 1)
+			indices.push(p2 + 1)
+		}
+	}
+	console.log(indices)
+
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 
 	// Create and load image used as texture
 	var image = new Image();
-	image.src = "./wood_texture_simple.png";
-	image.onload = function() {
+	image.src = "./globe_texture.jpg";
+	image.onload = function () {
 		var texture = gl.createTexture();
 		gl.bindTexture(gl.TEXTURE_2D, texture);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
 		gl.generateMipmap(gl.TEXTURE_2D);
 
 		gl.activeTexture(gl.TEXTURE0);
@@ -193,7 +171,7 @@ window.onload = function() {
 
 	// Create matrix for projection
 	var projMatrix = mat4.create();
-	mat4.perspective(projMatrix, Math.PI/3, 1, 0.1, 100);
+	mat4.perspective(projMatrix, Math.PI / 3, 1, 0.1, 100);
 	var projLocation = gl.getUniformLocation(program, "projMatrix");
 	gl.uniformMatrix4fv(projLocation, false, projMatrix);
 
@@ -210,10 +188,10 @@ window.onload = function() {
 	window.requestAnimationFrame = window.requestAnimationFrame
 		|| window.mozRequestAnimationFrame
 		|| window.webkitRequestAnimationFrame
-		|| function(cb) { setTimeout(cb, 1000/60); };
+		|| function (cb) { setTimeout(cb, 1000 / 60); };
 
-	var render = function() {
-		mat4.rotateX(modelMatrix, modelMatrix, 0.005);
+	var render = function () {
+		mat4.rotateX(modelMatrix, modelMatrix, 0.00);
 		mat4.rotateY(modelMatrix, modelMatrix, 0.01);
 		gl.uniformMatrix4fv(modelLocation, false, modelMatrix);
 
